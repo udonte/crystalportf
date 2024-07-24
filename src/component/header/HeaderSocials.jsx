7f604cd8b70b650d1aec44e55e3d4b1cadab2cde
George Udonte <georgeudonte@gmail.com>
2024-07-24 14:16:58 +0100

    fixed error

--- src/component/header/HeaderSocials.jsx
+++ src/component/header/HeaderSocials.jsx
@@ -7,9 +7,7 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/christianah-atunbi-7aa36723a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
         <BsLinkedin />{" "}
       </a>
-      <a href="https://twitter.com/GeorgeUdonte">
-        <img src="" />
-      </a>
+      <a href="https://twitter.com/GeorgeUdonte"></a>
     </div>
   );
 };
